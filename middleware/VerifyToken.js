7f9f9bba159135154a83b0377b5704032ef151c6
alirezaporyegane <alirezaporyegane@gmail.com>
2021-09-24 15:55:31 +0330

    fix

---
+++ middleware/VerifyToken.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+
+const Auth = (req, res, next) => {
+  const authHeaders = req.Headers.token
+  if (authHeaders) {
+    jwt.verify(authHeaders, process.env.TOKEN_SEC, (err, user) => {
+      if (err) return res.status(401).json({
+        msg: 'token is not valid',
+        error: err,
+        code: 401
+      })
+
+      req.user = user
+      next()
+    })
+  } else {
+    return res.status(401).json({
+      msg: 'you Are not authentication',
+      code: 401
+    })
+  }
+}
+
+const authentication = (req, res, next) => {
+  Auth(req, res, () => {
+    if (req.body.id = req.params.id || req.user.isAdmin) {
+      next()
+    } else {
+      res.status(403).json({
+        msg: 'you are not allow to that!',
+        code: 403
+      })
+    }
+  })
+}
+
+module.exports = { Auth, authentication }
--- routes/User.js
+++ routes/User.js
@@ -1,4 +1,31 @@
 const router = require('express').Router();
+const { Auth, authentication } = require('../middleware/VerifyToken')
+const cryptoJS = require('crypto-js');
+const User = require('../modules/User');
+const _ = require('lodash');
 
+
+router.put('/:id', [Auth, authentication], (req, res) => {
+  const password = req.body.password
+  if (password) {
+    password = cryptoJS.AES.encrypt(
+      password,
+      process.env.PASS_SEC
+    ).toString()
+  }
+
+  User.findByIdAndUpdate(req.params.id, {
+    $set: req.body
+  }, { new: true })
+    .then(result => {
+      res.status(200).json(_.pick(result, ['username', 'email']))
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err,
+        code: 500
+      })
+    })
+})
 
 module.exports = router
