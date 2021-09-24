f0940f49393b0f12ec038b2413a616713ee4648a
alirezaporyegane <alirezaporyegane@gmail.com>
2021-09-24 14:32:22 +0330

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,18 @@ const _ = require('lodash');
 const jwt = require('jsonwebtoken')
 // REGISTER
 router.post('/register', (req, res) => {
+  const newUser = new User(_.pick(req.body, ['username', 'email', 'password']))
 
+  newUser.save()
+    .then(result => {
+      res.status(200).json(_.pick(result, ['username', 'email']))
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err,
+        code: 500
+      })
+    })
 })
 
 
