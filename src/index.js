b50e04139b840bb88cdb25706cd8a36e21e14b92
alirezaporyegane <alirezaporyegane@gmail.com>
2021-09-25 19:50:46 +0330

    fix

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config()
+
+
+class App {
+  constructor () {
+    this.setupMongoDb()
+    this.setupMiddleware()
+    this.setupRoutes()
+    this.setupPort()
+  }
+
+  setupMongoDb () {
+    mongoose.connect("mongodb://127.0.0.1:27017/eCommers",{
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+      .then(() => {
+        console.log("mongodb Is Connected");
+      })
+      .catch(err => {
+        console.log("db not connected", err);
+      })
+  }
+
+  setupPort () {
+    const port = process.env.port || 5000;
+    app.listen(port, (err) => {
+      if (err) console.log(err);
+      else console.log(`app listen to port ${port}`)
+    });
+  }
+
+  setupMiddleware () {
+    const cors = require('cors');
+
+    app.use(cors());
+    app.use(express.json());
+  }
+
+  setupRoutes () {
+    const Api = require('./src/routes/Api');
+
+    app.use('/api', Api);
+  }
+}
+
+      
+
+
+
+
