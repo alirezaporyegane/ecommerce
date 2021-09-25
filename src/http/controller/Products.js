1b75323a638a0048c828c23d742020c8227df462
alirezaporyegane <alirezaporyegane@gmail.com>
2021-09-25 20:31:20 +0330

    fix

---
+++ src/http/controller/Products.js
@@ -0,0 +1,128 @@
+const Products = require('../modules/Products');
+const _ = require('lodash');
+
+
+class productsContrller {
+  async getAll (req, res) {
+    const qNew = req.query.new
+    const qCategories = req.query.categories
+    
+    try {
+      let products;
+
+      if (qNew) {
+        products = await Products.find().sort({ createdAt: -1 }).limit(5)
+      } else if (qCategories) {
+        products = await Products.find({ categories: {
+          $in: [qCategories]
+        } })
+      } else {
+        products = await Products.find()
+      }
+
+      res.status(200).json(products)
+    } catch (err) {
+      res.status(500).json({
+        error: err,
+        code: 500
+      })
+    }
+  }
+
+  async getCount (req, res) {
+    Products.find().countDocuments()
+      .then(result => {
+        res.status(200).json(result)
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err,
+          code: 500
+        })
+      })
+  }
+
+  async getById (req, res) {
+    const id = req.params.id
+
+    if (!mongoose.isValidObjectId(id)) 
+      return res.status(400).json({
+        msg: 'Request Faild',
+        code: 400
+      })
+
+    Products.findById(id)
+      .then(result => {
+        res.status(200).json(_.pick(result, ['title', 'desc', 'img', 'categories', 'size', 'color', 'price']))
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err,
+          code: 500
+        })
+      })
+  }
+
+  async create (req, res) {
+    const products = new Products(_.pick(req.body,  ['title', 'desc', 'img', 'categories', 'size', 'color', 'price' ]))
+  
+    products.save()
+      .then(result => {
+        res.status(200).json(_.pick(result, ['title', 'desc', 'img', 'categories', 'size', 'color', 'price']))
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err,
+          code: 500
+        })
+      })
+  }
+
+  async update (req, res) {
+    const id = req.params.id
+
+    if (!mongoose.isValidObjectId(id)) 
+      return res.status(400).json({
+        msg: 'Request Faild',
+        code: 400
+      })
+
+    products.findByIdAndUpdate(id, {
+        $set: req.body
+      }, { new: true })
+        .then(result => {
+          res.status(200).json(_.pick(result, ['title', 'desc', 'img', 'categories', 'size', 'color', 'price']))
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err,
+            code: 500
+          })
+        })
+  }
+
+  async delete (req, res) {
+    const id = req.params.id
+
+    if (!mongoose.isValidObjectId(id)) 
+      return res.status(400).json({
+        msg: 'Request Faild',
+        code: 400
+      })
+
+    products.findByIdAndDelete(id)
+      .then(() => {
+        res.status(200).json({
+          msg: 'user is delete'
+        })
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err,
+          code: 500
+        })
+      })
+  }
+}
+
+module.exports = new productsContrller()
--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,97 +1,25 @@
 const router = require('express').Router();
+const Products = require('../http/controller/Products')
 const { Auth, authentication, Admin } = require('../http/middleware/VerifyToken')
-const Products = require('../modules/Products');
-const _ = require('lodash');
 
-// CREATE PRODUCTS
-router.post('/', [Auth, Admin], (req, res) => {
-  const products = new Products(_.pick(req.body,  ['title', 'desc', 'img', 'categories', 'size', 'color', 'price' ]))
-  
-  products.save()
-    .then(result => {
-      res.status(200).json(_.pick(result, ['title', 'desc', 'img', 'categories', 'size', 'color', 'price']))
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err,
-        code: 500
-      })
-    })
-})
-
-// UPDATE PRODUCTS
-router.put('/:id', [Auth, authentication], (req, res) => {
-  products.findByIdAndUpdate(req.params.id, {
-    $set: req.body
-  }, { new: true })
-    .then(result => {
-      res.status(200).json(_.pick(result, ['title', 'desc', 'img', 'categories', 'size', 'color', 'price']))
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err,
-        code: 500
-      })
-    })
-})
-
-// DELETE PRODUCTS
-router.delete('/:id', [Auth, authentication], (req, res) => {
-  products.findByIdAndDelete(req.params.id)
-    .then(() => {
-      res.status(200).json({
-        msg: 'user is delete'
-      })
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err,
-        code: 500
-      })
-    })
-})
-
-
-// GET ONE PRODUCTS
-router.get('/:id', [Auth, authentication, Admin], (req, res) => {
-  Products.findById(req.params.id)
-    .then(result => {
-      res.status(200).json(_.pick(result, ['title', 'desc', 'img', 'categories', 'size', 'color', 'price']))
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err,
-        code: 500
-      })
-    })
-})
 
 // GET ALL PRODUCTS
-router.get('/', [Auth, Admin], async (req, res) => {
-  const qNew = req.query.new
-  const qCategories = req.query.categories
-  
-  try {
-    let products;
+router.get('/', [Auth, Admin], Products.getAll)
 
-    if (qNew) {
-      products = await Products.find().sort({ createdAt: -1 }).limit(5)
-    } else if (qCategories) {
-      products = await Products.find({ categories: {
-        $in: [qCategories]
-      } })
-    } else {
-      products = await Products.find()
-    }
+// GET COUNT
+router.get('/', [Auth, Admin], Products.getCount)
 
-    res.status(200).json(products)
-  } catch (err) {
-    res.status(500).json({
-      error: err,
-      code: 500
-    })
-  }
-})
+// GET ONE PRODUCTS
+router.get('/:id', [Auth, authentication, Admin], Products.getById)
+
+// CREATE PRODUCTS
+router.post('/', [Auth, Admin], Products.create)
+
+// UPDATE PRODUCTS
+router.put('/:id', [Auth, authentication], Products.update)
+
+// DELETE PRODUCTS
+router.delete('/:id', [Auth, authentication], Products.delete)
 
 
 module.exports = router
